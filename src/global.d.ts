4d0c4b603b2b03558ea1e0af38212d8091bcee49
Vladislav Selezov <vladislav.selezov@sgs.com>
2025-02-03 12:13:52 +0300

    add ?*raw import support

---
+++ src/global.d.ts
@@ -0,0 +1,16 @@
+declare module "*?raw" {
+	const content: string;
+	export default content;
+}
+
+// interface Window {
+// 	helloWorld(): void;
+// }
+
+// declare var window: Window;
+
+// declare var Math: Math;
+
+// interface Math {
+// 	seedrandom(seed?: string);
+// }
